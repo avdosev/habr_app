b8cb92cf0751b6386fc0de9ca89bebf0ac726997
avdosev <avdosev1@yandex.ru>
2020-10-04 20:57:27 +0400

    Fix spoiler block with long title

--- lib/widgets/spoiler_block.dart
+++ lib/widgets/spoiler_block.dart
@@ -15,12 +15,13 @@ class _SpoilerState extends State<Spoiler> with TickerProviderStateMixin {
   bool visible = false;
   Animation _arrowAnimation;
   AnimationController _arrowAnimationController;
+  static const duration = Duration(milliseconds: 300);
 
   @override
   initState() {
     super.initState();
     _arrowAnimationController =
-        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+        AnimationController(vsync: this, duration: duration);
     _arrowAnimation =
         Tween(begin: 0.0, end: pi/2).animate(_arrowAnimationController);
   }
@@ -47,22 +48,23 @@ class _SpoilerState extends State<Spoiler> with TickerProviderStateMixin {
                 animation: _arrowAnimationController,
                 builder: (context, child) => Transform.rotate(
                     angle: _arrowAnimation.value,
-                    child: Icon(Icons.arrow_right, color: themeData.primaryColor),
-                )
+                    child: child
+                ),
+                child: Icon(Icons.arrow_right, color: themeData.primaryColor),
               ),
-              Text(widget.title,
+              Expanded(child: Text(widget.title,
                 style: TextStyle(
                   color: themeData.primaryColor,
                   decorationColor: themeData.primaryColor,
                   decoration: TextDecoration.underline,
                   decorationStyle: TextDecorationStyle.dashed,
                 ),
-              ),
+              ))
             ],),
             onTap: onTap,
           ),
           if (visible) ...[
-            SizedBox(height: 10,),
+            const SizedBox(height: 10,),
             widget.child,
           ],
         ],
