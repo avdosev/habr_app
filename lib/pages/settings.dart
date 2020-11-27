3d2541595ed594b6bf987b653af31cb9bf496361
avdosev <avdosev1@yandex.ru>
2020-11-27 19:41:16 +0400

    remove DropDownTile

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -2,41 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
-class DropDownTile<Key> extends StatelessWidget {
-  final List<MapEntry<Key, String>> items;
-  final Function(Key key) onChanged;
-  final Key defaultKey;
-  final Widget title;
-  final Widget trailing;
-  final Widget leading;
-
-  DropDownTile({
-    Map<Key, String> values,
-    this.title,
-    this.trailing,
-    this.leading,
-    this.defaultKey,
-    this.onChanged,
-  }) : items = values.entries.toList();
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-        child: ListTile(
-      title: title,
-      trailing: DropdownButton<Key>(
-        value: defaultKey,
-        onChanged: onChanged,
-        items: items
-            .map<DropdownMenuItem<Key>>((item) =>
-                DropdownMenuItem<Key>(value: item.key, child: Text(item.value)))
-            .toList(),
-      ),
-      leading: leading,
-    ));
-  }
-}
-
 class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
