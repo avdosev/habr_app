844829ec9b95f4f2db883a7db5db9e515edf9226
avdosev <avdosev1@yandex.ru>
2021-01-18 21:49:09 +0300

    support offline svg

--- lib/widgets/picture.dart
+++ lib/widgets/picture.dart
@@ -29,48 +29,55 @@ class Picture extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Widget image;
     final textTheme = Theme.of(context).textTheme.bodyText2;
-
-    if (url.endsWith("svg")) {
-      image = Center(child: Transform.scale(
-        scale: 2,
-        alignment: Alignment.center,
-        child: SvgPicture.network(url, color: textTheme.color),
-      ));
-    } else {
-      image = Container(
-        height: height,
-        width: width,
-        child: LoadBuilder(
-          future: HabrStorage().imgStore.getImage(url),
-          onRightBuilder: (context, filePath) {
-            Widget widget = Image.file(
-              File(filePath),
-              height: height,
-              width: width,
-            );
-            if (clickable) {
-              widget = _buildClickableImage(
-                  context, widget, FileImage(File(filePath)));
-            }
-            return widget;
-          },
-          onErrorBuilder: (context, err) {
-            Widget widget = Image.network(
-              url,
-              height: height,
-              width: width,
-            );
-            if (clickable) {
-              widget = _buildClickableImage(context, widget, NetworkImage(url));
-            }
-            return widget;
-          },
-        ),
-      );
-    }
-    return image;
+    return Container(
+      height: height,
+      width: width,
+      child: LoadBuilder(
+        future: HabrStorage().imgStore.getImage(url),
+        onRightBuilder: (context, filePath) {
+          final file = File(filePath);
+          if (url.endsWith("svg")) {
+            // TODO: это костыль нужно нормально определять размер
+            return Center(child: Transform.scale(
+              scale: 2,
+              alignment: Alignment.center,
+              child: SvgPicture.file(file, color: textTheme.color),
+            ));
+          }
+          Widget widget = Image.file(
+            file,
+            height: height,
+            width: width,
+          );
+          if (clickable) {
+            widget = _buildClickableImage(
+                context, widget, FileImage(File(filePath)));
+          }
+          return widget;
+        },
+        onErrorBuilder: (context, err) {
+          logError(err);
+          if (url.endsWith("svg")) {
+            // TODO: это костыль нужно нормально определять размер
+            return Center(child: Transform.scale(
+              scale: 2,
+              alignment: Alignment.center,
+              child: SvgPicture.network(url, color: textTheme.color),
+            ));
+          }
+          Widget widget = Image.network(
+            url,
+            height: height,
+            width: width,
+          );
+          if (clickable) {
+            widget = _buildClickableImage(context, widget, NetworkImage(url));
+          }
+          return widget;
+        },
+      ),
+    );
   }
 
   _buildClickableImage(
