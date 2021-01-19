87ccbd81baa66d22946cf4cb0dd42e2d77d4ac8c
avdosev <avdosev1@yandex.ru>
2021-01-19 11:00:29 +0300

    add colors for default avatar

--- lib/styles/colors/default_avatar.dart
+++ lib/styles/colors/default_avatar.dart
@@ -12,7 +12,11 @@ class DefaultAvatarColors {
 
   static const grey = Color.fromRGBO(0x82, 0xa3, 0xb1, 1);
 
-  static const lightValues = values;
+  static const red = Color.fromRGBO(0xc6, 0x7b, 0x7b, 1);
+
+  static const orange = Color.fromRGBO(0xbd, 0xa4, 0x84, 1);
+
+  static const lightValues = [lilac, pink, green, blue, grey];
   static const darkValues = values;
-  static const values = [lilac, pink, green, blue, grey];
+  static const values = [lilac, pink, green, blue, grey, red, orange];
 }
