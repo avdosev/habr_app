066426166bc2c04a144ab35add5ec1e2ae7d4061
avdosev <avdosev1@yandex.ru>
2021-07-19 14:33:40 +0300

    fix: add appbar in imageview

--- lib/pages/image_view.dart
+++ lib/pages/image_view.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/services.dart';
 import 'package:photo_view/photo_view.dart';
 import 'package:flutter/material.dart';
 
@@ -18,13 +19,28 @@ class HeroPhotoViewRouteWrapper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: PhotoView(
-        imageProvider: imageProvider,
-        backgroundDecoration: backgroundDecoration,
-        minScale: minScale,
-        maxScale: maxScale,
-        heroAttributes: PhotoViewHeroAttributes(tag: tag),
+    final focusNode = FocusNode();
+    return RawKeyboardListener(
+      focusNode: focusNode,
+      autofocus: true,
+      onKey: (event) {
+        if (event.isKeyPressed(LogicalKeyboardKey.escape)) {
+          Navigator.of(context).pop();
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+        ),
+        body: Container(
+          child: PhotoView(
+            imageProvider: imageProvider,
+            backgroundDecoration: backgroundDecoration,
+            minScale: minScale,
+            maxScale: maxScale,
+            heroAttributes: PhotoViewHeroAttributes(tag: tag),
+          ),
+        ),
       ),
     );
   }
